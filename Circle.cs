770a5bb7010c4593d64ae05015739439e3b5464c
Edwin DeJesus <edwinstoddart.student@gmail.com>
2021-09-20 20:47:02 -0400

    Finished the assignment and all Bonuses.

---
+++ Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfACircle
+{
+    public static class Circle
+    {
+        public static double Area(double radius)
+        {
+            return Math.PI* Math.Pow(radius, 2);
+        }
+        public static double Circumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        public static double Diameter(double radius)
+        {
+            return 2 * radius;
+        }
+        public static double GallonsUsed(double circum, double MPG)
+        {
+            return circum / MPG;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,27 +6,38 @@ namespace AreaOfACircle
     {
         static void Main(string[] args)
         {
-            double radius;
-            double area;
-            double circum;
-            double diameter;
+            string UserInput = "-1";
+            double Radius;
+            double Area;
+            double Circum;
+            double Diameter;
 
-            Console.WriteLine("Enter the radius of a circle: ");
-            string userInput = Console.ReadLine();
-            radius = double.Parse(userInput);
+            while (Double.Parse(UserInput) < 0 || UserInput.Equals(""))
+            {
+                Console.WriteLine("Enter the radius of a circle: ");
+                UserInput = Console.ReadLine();
+                if (!Double.TryParse(UserInput, out Radius))
+                {
+                    Console.WriteLine("Error: Entered a Non-numerical string!!");
+                    Environment.Exit(0);
+                }
+            }
 
-            area = Math.PI * Math.Pow(radius, 2);
-            circum = 2 * Math.PI * radius;
-            diameter = 2 * radius;
+            Radius = Double.Parse(UserInput);
+            Area = Circle.Area(Radius);
+            Circum = Circle.Circumference(Radius);
+            Diameter = Circle.Diameter(Radius);
 
-            Console.WriteLine($"The area of a circle of radius {radius} is: {area} \n" +
-                $"The circumference of a circle of radius {radius} is: {circum} \n" +
-                $"The diameter of a circle of radius {radius} is: {diameter}");
+            Console.WriteLine($"The area of a circle of radius {Radius} is: {Area} \n" +
+                $"The circumference of a circle of radius {Radius} is: {Circum} \n" +
+                $"The diameter of a circle of radius {Radius} is: {Diameter}");
 
             Console.WriteLine("What is the MPG of your car: ");
-            userInput = Console.ReadLine();
+            UserInput = Console.ReadLine();
+            double MPG = Double.Parse(UserInput);
 
-
+            double GallonsUsed = Circle.GallonsUsed(Circum, MPG);
+            Console.WriteLine($"It would take you {GallonsUsed} gallons of gas to drive around this circle.");
         }
     }
 }
